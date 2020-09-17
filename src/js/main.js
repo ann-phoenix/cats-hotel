73df1b71b3632c84c1fe129301c8d2723aa34b08
ann-phoenix <ann.phoenix.it@gmail.com>
2020-09-17 21:53:34 +0300

    added rooms section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
 
+	/*Rooms slider  https://kenwheeler.github.io/slick/ */
+
+	$('.rooms__slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 9000,
+		prevArrow: '<button class="rooms__slider-btn rooms__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button > ',
+		nextArrow: '<button class="rooms__slider-btn rooms__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button > ',
+	});
 });
