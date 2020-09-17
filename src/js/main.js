0548591673f84ae71725f327cfafb887c3137891
ann-phoenix <ann.phoenix.it@gmail.com>
2020-09-17 22:53:37 +0300

    added reviews section

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,17 @@ $(function () {
 		prevArrow: '<button class="rooms__slider-btn rooms__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button > ',
 		nextArrow: '<button class="rooms__slider-btn rooms__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button > ',
 	});
+
+	/*Reviews slider  */
+
+	$('.reviews__slider').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 9000,
+		prevArrow: '<button class="reviews__slider-btn reviews__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button > ',
+		nextArrow: '<button class="reviews__slider-btn reviews__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button > ',
+	});
 });
