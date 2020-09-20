ef28d8938cce052eb71b7296dc7f2f8d7cd7ea5d
ann-phoenix <ann.phoenix.it@gmail.com>
2020-09-20 23:31:18 +0300

    added room-page

--- src/js/main.js
+++ src/js/main.js
@@ -26,4 +26,30 @@ $(function () {
 		prevArrow: '<button class="reviews__slider-btn reviews__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button > ',
 		nextArrow: '<button class="reviews__slider-btn reviews__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button > ',
 	});
+
+	$('.preview-class').slick({
+		arrows: false,
+		dots: false,
+		centerMode: false,
+		focusOnSelect: true,
+		vertical:true,
+		infinite:true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider-class'
+	});
+
+	$('.slider-class').slick({
+		arrows: false,
+		fade: true,
+		dots : false,
+		lazyLoad: 'progressive',
+		slidesToScroll:1,
+		slidesToShow:1,
+		infinite:true,
+		swipe:true,
+		asNavFor: '.preview-class'
+	});
+
+
 });
