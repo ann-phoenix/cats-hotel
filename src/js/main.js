0c9a4c98d2239392a031a0e75513955e2ab4129a
ann-phoenix <ann.phoenix.it@gmail.com>
2020-09-27 00:44:45 +0300

    added adaptive rooms slider

--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,16 @@ $(function () {
 		slidesToScroll: 1,
 		dots: true,
 		fade: true,
-		autoplay: true,
-		autoplaySpeed: 9000,
+		// autoplay: true,
+		// autoplaySpeed: 9000,
 		prevArrow: '<button class="rooms__slider-btn rooms__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button > ',
 		nextArrow: '<button class="rooms__slider-btn rooms__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button > ',
+		responsive: [{
+			breakpoint: 761,
+			settings: {
+				arrows: false,
+			}
+		}, ]
 	});
 
 	/* Reviews slider  */
